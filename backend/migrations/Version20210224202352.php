17ec024bb0decc08ecab4fc95e02ad582537f679
acode-writer <souhaibou.toure122@gmail.com>
2021-02-27 14:58:57 +0000

    migrations

---
+++ backend/migrations/Version20210224202352.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210224202352 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE transaction CHANGE frees fees DOUBLE PRECISION NOT NULL, CHANGE deposit_frees deposit_fees DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE transaction CHANGE fees frees DOUBLE PRECISION NOT NULL, CHANGE deposit_fees deposit_frees DOUBLE PRECISION DEFAULT NULL');
+    }
+}
