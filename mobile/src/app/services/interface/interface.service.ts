ce79d5ec4b9bb67c7bff1ddb481436267c0e124f
acode-writer <souhaibou.toure122@gmail.com>
2021-03-21 22:22:36 +0000

    update services.

--- mobile/src/app/services/interface/interface.service.ts
+++ mobile/src/app/services/interface/interface.service.ts
@@ -1,46 +1,104 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {UserInterface} from "../../models/user.interface";
-import {Observable, Subscription} from "rxjs";
+import {BehaviorSubject, from, Observable, Subscription} from "rxjs";
 import {environment} from "../../../environments/environment";
 import {JwtHelperService} from "@auth0/angular-jwt";
 import {ClientInterface} from "../../models/client.interface";
+import {LoginService} from "../login/login.service";
+import {map} from "rxjs/operators";
+import {log} from "util";
+import {TokenPayloadInterface} from "../../models/token-payload.interface";
+import {AlertController} from "@ionic/angular";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class InterfaceService {
-  public connectedUser: UserInterface;
+  public connectedUser = new BehaviorSubject(null);
   public userSubscription: Subscription;
+  user = new BehaviorSubject(null);
+  public solde = new BehaviorSubject(0);
   private _helper : JwtHelperService;
-  constructor(private http: HttpClient) {
+  public tokenPayload: TokenPayloadInterface;
+  public _isAdmin = new BehaviorSubject(false);
+  private role = 'ROLE_ADMIN_AGENCE';
+  private loginPath = '/login';
+  constructor(private http: HttpClient, private loginService: LoginService,
+              private alertCtrl: AlertController, private router: Router) {
     this._helper = new JwtHelperService();
   }
 
-  getTokenPayload(){
-    const token = localStorage.getItem(environment.token);
+  getTokenPayload(token: string){
     return this._helper.decodeToken(token);
   }
   get helper() {
     return this._helper;
   }
-  getConnectedUser(id: number){
-    this.userSubscription = this.getUser(id)
-      .subscribe(
-        (user: UserInterface) => {
-          this.connectedUser = user;
-        }
-      );
+  async getPayload() {
+     this.loginService.getData(environment.token).then(
+      token => {
+        const payload = this.getTokenPayload(token);
+        this.tokenPayload = payload;
+        this.user.next(payload);
+        this._isAdmin.next(this.isAdmin(payload.roles[0]));
+      }
+    )
   }
-  isAdmin() {
-    const token = localStorage.getItem(environment.token);
-    const decodedToken = this._helper.decodeToken(token);
-    return decodedToken.roles[0] == 'ROLE_ADMIN_AGENCE';
+  async saveConnectedUser(id: number){
+    await this.getUserById(id).subscribe(
+      response => {
+        this.connectedUser.next(response);
+        return  this.loginService.setData(environment.connectedUser,response);
+      }
+    )
   }
-  getUser(id: number): Observable<UserInterface> {
+  async getConnectedUser(){
+    await this.loginService.getData(environment.token).then(
+      token => {
+        const payload = this.getTokenPayload(token);
+        this.userSubscription = this.getUserById(payload.id).subscribe(
+          response => {
+              const solde = response?.agence?.account.balance || 0;
+              this.connectedUser.next(response);
+              this.solde.next(solde);
+              this._isAdmin.next(this.isAdmin(payload.roles[0]));
+              return  this.loginService.setData(environment.connectedUser,response);
+          },
+          error => {
+            if ( error.status == 401){
+              this.alertError('Session Expired','Votre session a expiré. Veuillez vous reconnecter.');
+            }
+          }
+        )
+      }
+    )
+  }
+  isAdmin(role: string) {
+    return role == this.role;
+  }
+  getUserById(id: number): Observable<UserInterface> {
     return  this.http.get<UserInterface>(`${environment.api}/admin/users/${id}`);
   }
   getClientByNci(nicNumber: string): Observable<ClientInterface> {
     return this.http.get<ClientInterface>(`${environment.api}/user/client/nci/${nicNumber}`);
+  }
+  getClientByPhoneNumber(phoneNumber: string): Observable<ClientInterface> {
+    return this.http.get<ClientInterface>(`${environment.api}/user/client/phone-number/${phoneNumber}`);
+  }
+  async alertError(title: string, message: string){
+    const alertDisplayer = await this.alertCtrl.create({
+      header: title,
+      message: message,
+      buttons: [
+        {
+          text: 'Se reconnecter',
+          handler: () => this.router.navigateByUrl(this.loginPath)
+        }
+      ]
+    });
+    await alertDisplayer.present();
   }
 }
--- mobile/src/app/services/login/login.service.ts
+++ mobile/src/app/services/login/login.service.ts
@@ -3,18 +3,37 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
 import {Storage} from "@ionic/storage";
+import {Platform} from "@ionic/angular";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor(private http: HttpClient, private storage: Storage) { }
+  constructor(private http: HttpClient, private storage: Storage, private platform: Platform) { }
 
   login(data: {username, password}): Observable<any> {
     return this.http.post(`${environment.api}/login_check`, data);
   }
   async setData(key, value) {
-    return  await this.storage.set(key, value);
+    if(this.platform.is('hybrid')){
+      return  await this.storage.set(key, value);
+    }else {
+      return await localStorage.setItem(key,value);
+    }
+  }
+  async  getData(key) {
+    if (this.platform.is('hybrid')){
+      return  this.storage.get(key);
+    }else {
+      return  localStorage.getItem(key);
+    }
+  }
+  async logout(){
+    if (this.platform.is('hybrid')){
+      await this.storage.remove(environment.connectedUser);
+      return await this.storage.remove(environment.token);
+    }
+    await localStorage.removeItem(environment.connectedUser);
+    return await localStorage.removeItem(environment.token);
   }
 }
